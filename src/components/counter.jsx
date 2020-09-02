8e792aad34c90025dd584403ae135b5f45103790
Shaheen <shaheenniaz582@gmail.com>
2020-09-02 11:20:26 +0100

    set up 2 components

---
+++ src/components/counter.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    state = { 
+        count: 0
+     };
+     handleIncrement = () => {
+         this.setState({ count: this.state.count + 1})
+     }
+     getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+    
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero': count;
+    }  
+    render() { 
+        return (  
+            <div>
+              <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+              <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Incement</button>
+              </div>
+        );
+    }
+}
+ 
+export default Counter;
